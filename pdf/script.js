9215643ba011edf91298a39c6612045a0db22f93
Adem Erbas <ae@netsense.no>
2023-12-07 01:33:21 +0100

    report-csv-excel-printout-pdf

---
+++ pdf/script.js
@@ -0,0 +1,97 @@
+
+let btn = document.getElementById("cmd");
+
+btn.addEventListener("click", function()
+{
+	
+ 
+	window.jsPDF = window.jspdf.jsPDF;
+	let pdf = new jsPDF('p', 'mm', 'a4');
+	
+	pdf.page=1;
+
+	let elementHTML = document.querySelector("#container");
+	console.log("elementHTML; ", elementHTML);
+
+
+// doc.text('Hello, this is your PDF content!', 10, 10);
+
+
+
+pdf.html(elementHTML, {
+	callback: function(doc) {
+	  
+  window.open(doc.output('bloburl'), '_blank');
+  //window.open(doc.output('bloburl'))
+  //doc.output('dataurlnewwindow');
+  //window.open(url,"_blank","top=100,left=200,width=1000,height=500");
+  
+  },
+  autoPaging: 'text',
+  x: 15,
+  y: 15,
+
+  width: 170, //target width in the PDF document
+  windowWidth: 650 //window width in CSS pixels			
+});
+
+
+
+
+const blob = doc.output('blob');
+const url = URL.createObjectURL(blob);
+window.open(url, '_blank');
+
+	
+})
+
+/*
+
+generatePdf():   {
+const doc = new jsPDF();
+doc.text('Hello, this is your PDF content!', 10, 10);
+const blob = doc.output('blob');
+const url = URL.createObjectURL(blob);
+window.open(url, '_blank');
+}
+
+
+
+
+
+console.log("Working!")
+	//renderer- 
+	
+	window.jsPDF = window.jspdf.jsPDF;
+
+	let pdf = new jsPDF('p', 'mm', 'a4');
+
+	pdf.page=1;
+	
+
+	// Source HTMLElement or a string containing HTML.
+
+	let elementHTML = document.querySelector("#container");
+	console.log("elementHTML; ", elementHTML);
+
+
+	pdf.html(elementHTML, {
+		 callback: function(doc) {
+
+	
+			
+		window.open(doc.output('bloburl'), '_blank');
+		//window.open(doc.output('bloburl'))
+		//doc.output('dataurlnewwindow');
+		//window.open(url,"_blank","top=100,left=200,width=1000,height=500");
+		
+		},
+		autoPaging: 'text',
+		x: 15,
+		y: 15,
+
+		width: 170, //target width in the PDF document
+		windowWidth: 650 //window width in CSS pixels			
+	});
+
+*/
